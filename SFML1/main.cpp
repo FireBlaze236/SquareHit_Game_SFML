5fa4cd86b53660811c5e582620593d09f5ea98fe
M. K. Bashar <bashar.onion@gmail.com>
2020-09-04 01:16:10 +0600

    Documentation & Fix file path bug for Linux

--- SFML1/main.cpp
+++ SFML1/main.cpp
@@ -1,3 +1,8 @@
+/*
+		This Project is done as Academic Project 
+	for 3rd Semester Object Oriented Programming Course
+*/
+
 #include "Game.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -7,47 +12,22 @@
 
 int main()
 {
-	//Placeholder UI
-	/*std::cout << "Welcome to Square Hit! Press any key to continue..." <<std::endl;
-	std::cin.get();*/
-	int ms,cs,cn,d;
-
-	ms = 71;
-	cs = 1 + (rand()%420);
-	cn = 1;
-	d = 1;
-
-	/*std::cout << "Enter Map Seed : ";
-	std::cin >> ms;
-	
-	std::cout << "Enter number of colors (default 3, max 10): ";
-	std::cin >> cn;
-
-	std::cout << "Enter Color Generate Seed : ";
-	std::cin >> cs;
-	
-	
-	std::cout << "Enter Difficulty multiplier (default 1, max 5): ";
-	std::cin >> d;*/
-
-	//std::cout << "Game loaded!" << std::endl;
-	//std::cout << "Enjoy!";
-
-	//sf::RenderWindow window(sf::VideoMode(512, 512), "Test 32");
-
-	Game game(512, 512, "SquareHit", ms, cn, cs, d);
+	// Creating Game & Menu Object Simultaneouly
+	Game game(512, 512, "SquareHit", 71, 1 + (rand() % 420), 1, 1);
 	Menu menu(*(game.gameWindow));
 
 	game.gameWindow->setVisible(true);
+	
 	// Game Loop
 	while (game.IsRunning())
 	{
-		
+		//MainMenu
 		if (game.GameMainMenu == 1)
 		{
 			menu.Update(game.GameMainMenu);
 			menu.draw(0, 4);
 		}
+		//Game Loop
 		else if (game.GameMainMenu == 0) 
 		{
 			game.HandleEvents();
@@ -55,11 +35,19 @@ int main()
 				game.Update();
 			game.Render();
 		}
+		//Option Menu
 		else if (game.GameMainMenu == 2)
 		{
 			menu.OptionUpdate(game.GameMainMenu, game);
 			menu.draw(4, 7);
 		}
+		//Credit
+		else if (game.GameMainMenu == 3)
+		{
+			
+			break;
+		}
+		//Exit
 		else
 		{
 			break;
@@ -67,18 +55,6 @@ int main()
 	}
 
 	game.gameWindow->setVisible(false);
-
-	/*
-
-		//window.clear(sf::Color::Black);
-
-		//window.draw(sprite);
-
-		menu.draw(window);
-
-		window.display();
-	}
-	*/
 
     return 0;
 }
